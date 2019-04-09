e87f88e0a26da427c6daeb0abb9202136ecefc78
Tim van Iersel <tim@websitescanner.io>
2019-04-09 15:52:36 +0200

    Remove boilerplate CSS JS and add functions

--- public/class-remove-schema-public.php
+++ public/class-remove-schema-public.php
@@ -1,100 +1,129 @@
 <?php
 
 /**
- * The public-facing functionality of the plugin.
- *
- * @link       https://timvaniersel.com/
- * @since      1.0.0
- *
- * @package    Remove_Schema
- * @subpackage Remove_Schema/public
- */
+* The public-facing functionality of the plugin.
+*
+* @link       https://timvaniersel.com/
+* @since      1.0.0
+*
+* @package    Remove_Schema
+* @subpackage Remove_Schema/public
+*/
 
 /**
- * The public-facing functionality of the plugin.
- *
- * Defines the plugin name, version, and two examples hooks for how to
- * enqueue the public-facing stylesheet and JavaScript.
- *
- * @package    Remove_Schema
- * @subpackage Remove_Schema/public
- * @author     Tim van Iersel <tim@websitescanner.io>
- */
+* The public-facing functionality of the plugin.
+*
+* Defines the plugin name, version, and two examples hooks for how to
+* enqueue the public-facing stylesheet and JavaScript.
+*
+* @package    Remove_Schema
+* @subpackage Remove_Schema/public
+* @author     Tim van Iersel <tim@websitescanner.io>
+*/
 class Remove_Schema_Public {
 
 	/**
-	 * The ID of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $plugin_name    The ID of this plugin.
-	 */
+	* The ID of this plugin.
+	*
+	* @since    1.0.0
+	* @access   private
+	* @var      string    $plugin_name    The ID of this plugin.
+	*/
 	private $plugin_name;
 
 	/**
-	 * The version of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $version    The current version of this plugin.
-	 */
+	* The version of this plugin.
+	*
+	* @since    1.0.0
+	* @access   private
+	* @var      string    $version    The current version of this plugin.
+	*/
 	private $version;
 
 	/**
-	 * Initialize the class and set its properties.
-	 *
-	 * @since    1.0.0
-	 * @param      string    $plugin_name       The name of the plugin.
-	 * @param      string    $version    The version of this plugin.
-	 */
+	* Initialize the class and set its properties.
+	*
+	* @since    1.0.0
+	* @param      string    $plugin_name       The name of the plugin.
+	* @param      string    $version    The version of this plugin.
+	*/
 	public function __construct( $plugin_name, $version ) {
 
 		$this->plugin_name = $plugin_name;
 		$this->version = $version;
+		$this->remove_schema_options = get_option($this->plugin_name);
 
 	}
 
+	// Remove all JSON0ld
+	public function remove_schema_jsonld() {
+		if(!empty($this->remove_schema_options['rm_jsonld'])){
+
+		}
+	}
+
+	// Remove all Yoast JSON-ld
+	public function remove_schema_yoast_jsonld() {
+		if(!empty($this->remove_schema_options['yoast_jsonld'])){
+
+		}
+	}
+
+	// Remove all JSON ld
+	public function remove_schema_yoast_jsonld() {
+		if(!empty($this->remove_schema_options['microdata'])){
+
+		}
+	}
+
+	// Remove all JSON ld
+	public function remove_schema_yoast_jsonld() {
+		if(!empty($this->remove_schema_options['rdfa'])){
+
+		}
+	}
+
 	/**
-	 * Register the stylesheets for the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
+	* Register the stylesheets for the public-facing side of the site.
+	*
+	* @since    1.0.0
+	*/
 	public function enqueue_styles() {
 
 		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Remove_Schema_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Remove_Schema_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
+		* This function is provided for demonstration purposes only.
+		*
+		* An instance of this class should be passed to the run() function
+		* defined in Remove_Schema_Loader as all of the hooks are defined
+		* in that particular class.
+		*
+		* The Remove_Schema_Loader will then create the relationship
+		* between the defined hooks and the functions defined in this
+		* class.
+		*/
 
 		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/remove-schema-public.css', array(), $this->version, 'all' );
 
 	}
 
 	/**
-	 * Register the JavaScript for the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
+	* Register the JavaScript for the public-facing side of the site.
+	*
+	* @since    1.0.0
+	*/
 	public function enqueue_scripts() {
 
 		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Remove_Schema_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Remove_Schema_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
+		* This function is provided for demonstration purposes only.
+		*
+		* An instance of this class should be passed to the run() function
+		* defined in Remove_Schema_Loader as all of the hooks are defined
+		* in that particular class.
+		*
+		* The Remove_Schema_Loader will then create the relationship
+		* between the defined hooks and the functions defined in this
+		* class.
+		*/
 
 		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/remove-schema-public.js', array( 'jquery' ), $this->version, false );
 
