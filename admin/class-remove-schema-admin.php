08de3eefcfd3cd2dbec92b6efd06182511b2d94c
Lorenzo Nannings <lorenzonannings@gmail.com>
2019-04-15 15:33:50 +0200

    Added support for schema pro

--- admin/class-remove-schema-admin.php
+++ admin/class-remove-schema-admin.php
@@ -136,6 +136,7 @@ public function validate($input) {
     $valid['yoast_jsonld'] = (isset($input['yoast_jsonld']) && !empty($input['yoast_jsonld'])) ? 1: 0;
 		$valid['woocommerce_jsonld'] = (isset($input['woocommerce_jsonld']) && !empty($input['woocommerce_jsonld'])) ? 1: 0;
 		$valid['woocommerce_mail_jsonld'] = (isset($input['woocommerce_mail_jsonld']) && !empty($input['woocommerce_mail_jsonld'])) ? 1: 0;
+		$valid['schema_pro'] = (isset($input['schema_pro']) && !empty($input['schema_pro'])) ? 1: 0;
     $valid['microdata'] = (isset($input['microdata']) && !empty($input['microdata'])) ? 1 : 0;
     $valid['rdfa'] = (isset($input['rdfa']) && !empty($input['rdfa'])) ? 1 : 0;
 
